ab2fad18f79b8ba69d5b5bb222d49e9dc8f5bb87
kuarisu <kuarisuran@gmail.com>
2016-10-17 17:15:48 +0200

    1st sycnh
    Slide List & RP
    Clicking Area and Score

---
+++ Assets/Script/Manager_Score.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Manager_Score : MonoBehaviour
+{
+    public int m_PreviousScore;
+    Text m_Text;                      // Reference to the Text component.
+
+
+    void Awake()
+    {
+        m_Text = GetComponent<Text>();
+        m_PreviousScore = 0;
+        ChangeScore(0);
+    }
+
+
+    public void ChangeScore(int _score)
+    {
+        
+        int _currentScore = m_PreviousScore + _score;
+        m_Text.text = "Ressources: " + _currentScore;
+        m_PreviousScore = _currentScore;
+    }
+}
+
---
+++ Assets/Script/Mouse_Click.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouse_Click : MonoBehaviour {
+
+
+    public GameObject m_MainScreen;
+    public GameObject m_RessourcesScore;
+    public int m_ClickGain;
+
+    bool m_isClickedList = false;
+    bool m_isClickedRP = false;
+
+    int m_Score;
+
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+            CheckRayCast();
+    }
+
+    void CheckRayCast()
+    {
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit _hit;
+
+        if (Physics.Raycast(_ray, out _hit))
+        {
+            #region Onglets
+            if (_hit.collider.tag == "TriggerList")
+            {
+                if (m_isClickedList == false)
+                {
+                    m_MainScreen.GetComponent<Screen_Slide>().LerpList();
+                    m_isClickedList = true;
+                }
+
+                else
+                {
+                    m_MainScreen.GetComponent<Screen_Slide>().LerpMain();
+                    m_isClickedList = false;
+                }
+
+
+            }
+
+            if (_hit.collider.tag == "TriggerRP")
+            {
+                if (m_isClickedRP == false)
+                {
+                    m_MainScreen.GetComponent<Screen_Slide>().LerpRP();
+                    m_isClickedRP = true;
+                }
+
+                else
+                {
+                    m_MainScreen.GetComponent<Screen_Slide>().LerpMain();
+                    m_isClickedRP = false;
+                }
+            }
+            #endregion
+
+            if (_hit.collider.tag == "ClickArea")
+            {
+                m_RessourcesScore.GetComponent<Manager_Score>().ChangeScore(m_ClickGain);
+            }
+        }
+    }
+}
